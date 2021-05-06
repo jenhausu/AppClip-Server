9ddf4234330513436ee69209a7095d2b37609595
蘇健豪 <jenhausu@osensetech.com>
2021-05-06 12:12:33 +0800

    feat: add UniversalLinksMiddleware
    what: custom response header
    why: apple-app-site-association don't have .json extension

---
+++ Sources/App/Middleware/UniversalLinksMiddleware.swift
@@ -0,0 +1,19 @@
+//
+//  UniversalLinksMiddleware.swift
+//  
+//
+//  Created by 蘇健豪 on 2021/5/6.
+//
+
+import Vapor
+
+struct UniversalLinksMiddleware: Middleware {
+        
+    func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+        next.respond(to: request).map { response in
+            response.headers.add(name: "content-type", value: "application/json")
+            return response
+        }
+    }
+    
+}
