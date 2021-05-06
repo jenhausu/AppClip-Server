08b8cc4d2b0ed9eb427a38dc55b3a6d4e7814d24
蘇健豪 <jenhausu@osensetech.com>
2021-05-06 17:02:39 +0800

    feat: only modify header when request AASA

--- Sources/App/Middleware/UniversalLinksMiddleware.swift
+++ Sources/App/Middleware/UniversalLinksMiddleware.swift
@@ -10,7 +10,11 @@ import Vapor
 struct UniversalLinksMiddleware: Middleware {
     
     func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
-        next.respond(to: request).map { response in
+        guard request.url.string == "/.well-known/apple-app-site-association" else {
+            return next.respond(to: request)
+        }
+        
+        return next.respond(to: request).map { response in
             response.headers.add(name: "content-type", value: "application/json")
             return response
         }
